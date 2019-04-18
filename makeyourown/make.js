a1caa92de9699b55aa40cfbde7cfddb5df746e4b
Heather Kim <haesoo1108@gmail.com>
2019-04-18 20:34:54 +0900

    added some more options, can choose only one glass

--- makeyourown/make.js
+++ makeyourown/make.js
@@ -24,10 +24,32 @@ for (i = 0; i < selectors.length; i++) {
 }
 
 var choiceItems = document.getElementsByClassName('choice-item');
-var glassItems = choiceItems.getElementsByClassName('glass');
+var glassItems = document.getElementsByClassName('glass');
 var j;
-for (j = 0; j<choiceItems.length; i++){
-    choiceItems[i].addEventListener("click", function(){
+for (j = 0; j<choiceItems.length; j++){
+    choiceItems[j].addEventListener("click", function(){
         this.classList.toggle("selected");
+        if (this.classList.contains('glass')){
+            let k;
+            for (k = 0; k<glassItems.length; k++){
+                if (glassItems[k] !== this && glassItems[k].classList.contains('selected')){
+                    glassItems[k].classList.toggle('selected');
+                }
+            }
+            showGlass(this.innerHTML.toLowerCase());
+        }
     })
+}
+
+function showGlass(string){
+    let k;
+    let glass = document.getElementById(string);
+    let glassArray = document.getElementsByClassName('glasstype')
+    glass.style.display = "block";
+    for (k = 0; k<glassItems.length; k++){
+        if (!glassItems[k].classList.contains('selected')){
+            glassArray[k].style.display = "none";
+            console.log("nullified")
+        }
+    }
 }

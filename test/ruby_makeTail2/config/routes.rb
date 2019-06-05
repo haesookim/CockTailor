64a5ec5e71b7171a32db5a0baa30decbd0657ea1
SeokMinchang <seokminchang21@gmail.com>
2019-06-05 11:02:20 +0900

    first commit

--- test/ruby_makeTail2/config/routes.rb
+++ test/ruby_makeTail2/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'makeyourowns/gallery'
   root 'welcome#index'
   get 'welcome/index'
+  resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

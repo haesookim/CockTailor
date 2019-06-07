22f68a17832d19117495ff616c46fc761a6c3c16
Heather Kim <haesoo1108@gmail.com>
2019-06-08 03:09:15 +0900

    (almost) implemented search and display

--- test/ruby_makeTail2/app/assets/javascripts/recommend.js
+++ test/ruby_makeTail2/app/assets/javascripts/recommend.js
@@ -1,7 +1,11 @@
 var answerString = ""; 
 
-function appendAnswer(object){
+function initialize(){
+    answerString = "";
     mySwipe.next();
+}
+
+function appendAnswer(object){
     if (object.classList.contains('L')){
         answerString+="0";
     } if (object.classList.contains('R')){
@@ -10,21 +14,22 @@ function appendAnswer(object){
         answerString+="02";
         mySwipe.next();
     } if (object.classList.contains('PL') || object.classList.contains('PL')){
-        if (answerString.substring(0, 2) === "02"){
+        if (answerString[0] == "0" && answerString[1] =="2"){
             if (object.classList.contains('PL')){
                 answerString+="02";
-            } if (object.classList.contains('PR')){
-                answerString+="12";
+            } else {
+                answerString+="12"; // this is not working somehow
             }
             mySwipe.next();
         } else{
             if (object.classList.contains('PL')){
                 answerString+="0";
             } if (object.classList.contains('PR')){
-                answerString+="1";
+                answerString+="1"; // this is not working either
             }
         }
     }
+    mySwipe.next();
     console.log(answerString);
     if (answerString.length == 6){
         findMatch(answerString);
@@ -34,8 +39,8 @@ function appendAnswer(object){
 function findMatch(string){
     for (let i = 0; i<cocktail_data.length; i++){
         if (string === cocktail_data[i].answerstring){
-            //load corresponding cocktail
-            console.log(cocktail_data[i].name);
+            document.getElementById('answer-title').innerHTML = cocktail_data[i].name;
+            document.getElementById('answer-text').innerHTML = cocktail_data[i].description;
             break;
         }
     }

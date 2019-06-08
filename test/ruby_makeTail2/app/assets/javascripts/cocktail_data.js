0c0c9acd43ccbfcb4dced3e70ba0019cb0f54ccc
hhjjpp <49246551+hhjjpp@users.noreply.github.com>
2019-06-09 00:40:32 +0900

    cocktail_data.js updated

--- test/ruby_makeTail2/app/assets/javascripts/cocktail_data.js
+++ test/ruby_makeTail2/app/assets/javascripts/cocktail_data.js
@@ -18,7 +18,9 @@ var cocktail_data = [
             }
         ],
         description: "미드 '빅뱅이론'에도 등장한 적 있는 '그래스호퍼'입니다.</br> 메뚜기라는 이름답게 녹색 빛을 띠는 이 칵테일은 크림이 들어가서 부드럽게 즐길 수 있습니다.</br> 민트초코 맛이 난다고 하니, 민초파 여러분들은 꼭 도전해보세요!",
-        cocktailColor: "#A6E8AC"
+        cocktailColor: "#A6E8AC",
+        glass: "martini",
+        garnish: "민트잎"
     },
     {
         name: "블랙벨벳",
@@ -35,7 +37,8 @@ var cocktail_data = [
             }
         ],
         description: "블랙벨벳은 1861년 영국 런던의 Brook's club에서 만들어졌습니다.</br> 당시 빅토리아 여왕의 부군이었던 알버트 왕자의 죽음을 애도하기 위해 만들어진 이 칵테일은 조문객들이 착용하는 검은 완장을 형상화한 것이라고 합니다.</br> 샴페인의 청량감에 스타우트 흑맥주의 쌉싸름함과 깊은 풍미가 느껴지는 고급스러운 맛을 느껴보세요.",
-        cocktailColor: "#F4DD7C, #430B0E"
+        cocktailColor: "#F4DD7C, #430B0E",
+        glass: "liqueur"
     },
     {
         name: "블러디메리",
@@ -73,7 +76,9 @@ var cocktail_data = [
         ],
         description: "토마토 주스, 핫소스, 소금, 후추를 넣고 샐러리를 꽂아 완성하는 다소 특이한 칵테일입니다. </br> \"피의 메리\"라는 섬뜩한 이름은 잉글랜드의 여왕 메리 1세의 별칭에서 따온 것으로, 피처럼 선명한 붉은 색 때문에 이런 이름이 붙었다고 하네요.</br> 레시피에서 술을 빼면 virgin mary, 보드카를 데킬라로 대체하면 bloody maria 로도 즐길 수 있습니다.",
 
-        cocktailColor: "#C13010"
+        cocktailColor: "#C13010",
+        glass: "collins",
+        garnish: "슬라이스 레몬, 샐러리"
     },
     {
         name: "핫 버터드 럼",
@@ -98,7 +103,9 @@ var cocktail_data = [
             }
         ],
         description: "이탈리아에서 오래전부터 즐겨 마시던 칵테일입니다.</br>럼의 진한 맛과 버터의 고소한 맛이 이루는 조화가 일품으로, 특히 추운 겨울이나 감기 기운이 있을 때 마시기 좋습니다.</br>레시피에서 물 대신 우유를 넣으면 Hot buttered rum cow가 됩니다.",
-        cocktailColor: "#DBA86A"
+        cocktailColor: "#DBA86A",
+        glass: "high-ball",
+        garnish: "슬라이스 레몬, 시나몬 스틱"
     },
     {
         name: "쿠바 리브레",
@@ -119,7 +126,9 @@ var cocktail_data = [
             }
         ],
         description: "쿠바리브레라는 이름은 스페인의 식민지였던 쿠바가 독립운동 당시 사용하던 \"Viva Cuba Libre(자유 쿠바 만세)\"라는 구호에서 유래되었습니다.</br> 쿠바의 독립전쟁을 지원하기 위해 하바나에 주둔하던 미군이 승리를 축하하며 쿠바산 럼에 미국산 콜라를 넣어 마신 것에서 탄생했습니다.</br> 럼 특유의 달콤한 향기에 콜라의 단맛과 레몬의 신맛이 가미되어 상큼함을 더해진 맛입니다.",
-        cocktailColor: "#843118"
+        cocktailColor: "#843118",
+        glass: "high-ball",
+        garnish: "라임 웨지"
     },
     {
         name: "러스티네일",
@@ -137,7 +146,9 @@ var cocktail_data = [
         ],
         description: "스카치위스키 베이스에 드람뷔 리큐르를 섞어 꿀의 묵직한 단맛을 느낄 수 있는 칵테일입니다.</br>금주법 시대에 사람들이 밀주의 조잡한 맛을 가리기 위해 드람뷔 리큐르를 섞어 마시기 시작한 데서 탄생하게 되었습니다.</br>'녹슨 못'이라는 이름 자체는 60년대에 붙은 것으로, 칵테일의 색을 비유한 이름이라고 합니다.",
 
-        cocktailColor: "#FFA621"
+        cocktailColor: "#FFA621",
+        glass: "whiskey",
+        garnish: "레몬 트위스트"
     },
     {
         name: "프렌치75",
@@ -164,7 +175,9 @@ var cocktail_data = [
 
         description: "프렌치75는 제1차 세계대전 중 파리에서 탄생한 칵테일입니다. 그 이름은 당시 프랑스에서 개발된 1897년식 75mm 대포에서 따왔습니다.</br>샴페인이 들어가며 약간 단맛을 느낄 수 있습니다.</br>베이스로 진 대신 버본 위스키를 사용할 경우 french 95, 브랜디를 사용할 경우 french 125 라는 칵테일로 즐길 수 있습니다."
         ,
-        cocktailColor: "#FAE79A"
+        cocktailColor: "#FAE79A",
+        glass: "liqueur",
+        garnish: "레몬 트위스트"
     },
     {
         name: "에그노그",
@@ -197,7 +210,9 @@ var cocktail_data = [
             }
         ],
         description: "우유, 계란, 설탕 등을 이용해 만드는 부드러운 음료로, 밀크쉐이크와 비슷한 에그노그 칵테일입니다.</br>술을 넣지 않고 무알콜 음료로 즐기기도 합니다.</br>영양가가 높고 따뜻해서 피로회복에 좋고 추운 날 마시기에 안성맞춤입니다.</br>많은 미국인들이 크리스마스 시즌에 즐겨마시는 음료로, 크리스마스가 다가오면 집집마다 풍기는 달달한 냄새의 정체가 바로 이 에그노그의 냄새라고 합니다."
-        , cocktailColor: "#EFE4CD"
+        , cocktailColor: "#EFE4CD",
+        glass: "high-ball",
+        garnish: "시나몬 스틱"
     },
     {
         name: "스키 리프트",
@@ -222,7 +237,9 @@ var cocktail_data = [
             }
         ],
         description: "스키리프트는 특이하게도 핫초코와 럼을 섞어 만드는 칵테일입니다.</br>보통 핫초코하면 초콜릿의 깊은 단 맛부터 떠올리기 쉽지만, 이 칵테일에는 피치 슈냅스와 코코넛 리큐르가 들어가 열대과일의 싱그러운 맛 또한 느낄 수 있습니다."
-        , cocktailColor: "#703712, #D7A168"
+        , cocktailColor: "#703712, #D7A168",
+        glass: "high-ball",
+        garnish: "휘핑한 크림"
     },
     {
         name: "오후의 죽음",
@@ -239,7 +256,8 @@ var cocktail_data = [
             }
         ],
         description: "오후의 죽음은 빈센트 반 고흐가 사랑했던 술로 유명한 압생트와 샴페인을 섞어 만드는 칵테일입니다.</br>미국의 문학가 헤밍웨이가 고안한 칵테일로, 자신의 단편인 「오후의 죽음」에서 이름을 따온 것으로 알려집니다.</br>압생트의 산초향과 샴페인의 청량감이 함께 느껴지는 오묘한 맛을 즐길 수 있습니다."
-        , cocktailColor: "#9AF193, #DEEF85"
+        , cocktailColor: "#9AF193, #DEEF85",
+        glass: "liqueur"
     },
     {
         name: "스크리밍 오르가즘",
@@ -260,7 +278,8 @@ var cocktail_data = [
             }
         ],
         description: "스크리밍 오르가즘은 영화 <칵테일(Cocktail)>을 통해 인기를 끌었던 칵테일인 오르가즘에 보드카를 추가해 만드는 칵테일입니다.</br>달콤한 커피향과 아몬드 리큐어, 부드러운 아이리쉬 크림이 어우러져 달콤하면서 부드러운 맛을 즐길 수 있습니다."
-        , cocktailColor: "#511120, #9C4B06, #CFC5A7, #E6E7DC"
+        , cocktailColor: "#511120, #9C4B06, #CFC5A7, #E6E7DC",
+        glass: "shot"
     },
     {
         name: "핫 샷",
@@ -282,7 +301,8 @@ var cocktail_data = [
         ],
         description: "바닐라 향이 나는 슈터 칵테일입니다.</br>도수가 높으며 한 번에 들이켜야 그 맛을 완전히 느낄 수 있습니다.</br>1987년에 처음 만들어진 이 칵테일은 90년대 초까지 선풍적인 인기를 끌었습니다."
         ,
-        cocktailColor: "#F8E201, #3F0E17, #F0F0F0"
+        cocktailColor: "#F8E201, #3F0E17, #F0F0F0",
+        glass: "shot"
     },
     {
         name: "허리케인",
@@ -320,7 +340,9 @@ var cocktail_data = [
         ],
         description: "허리케인은 1940년대 O'Briens가 뉴올리언스바 Pat O Briens에서 처음 만든 칵테일입니다.</br>그가 강매된 럼을 빨리 없애고자 이런 술을 만들었다고 전해집니다.</br>이 술은 뉴욕에서 열린 세계페어에서 데뷔하게 되는데, 그때 처음 서빙된 잔이 모양이 허리케인 램프의 모양이라고 해서 허리케인이라는 이름이 붙여졌습니다.</br>상쾌하고 깔끔한 맛을 즐길 수 있습니다."
         ,
-        cocktailColor: "#CC2B10, #F56626"
+        cocktailColor: "#CC2B10, #F56626",
+        glass: "high-ball",
+        garnish: "체리, 슬라이스 오렌지"
     },
     {
         name: "다이키리",
@@ -341,7 +363,9 @@ var cocktail_data = [
             }
         ],
         description: "다이키리의 시초는 쿠바 광산에 근무했던 제닝스 콕스라는 사람이 그를 찾아오는 친구들을 대접하려고 만든 칵테일이며 그 명칭은 쿠바의 광산 이름에서 이름지어졌습니다.</br>신맛과 단맛의 조화가 뛰어나며 럼을 사용한 대표적인 칵테일 중 하나입니다.</br>다이키리를 말할 때 빠지지 않는 것이 바로 다이키리 마니아인 미국의 문학가 어니스트 헤밍웨이입니다.</br>크러시드 아이스와 함께 믹서로 혼합하여 셔벗형태로 만들면 프로즌 다이키리로 즐길 수 있습니다.</br>이 프로즌 다이키리는 10년의 슬럼프에서 헤밍웨이를 구해 그 유명한 〈노인과 바다〉를 쓰게 했다고 전해집니다."
-        , cocktailColor: "#F0FFBD"
+        , cocktailColor: "#F0FFBD",
+        glass: "martini",
+        garnish: "슬라이스 라임"
     },
     {
         name: "데킬라 선라이즈",
@@ -362,7 +386,9 @@ var cocktail_data = [
             }
         ],
         description: "데킬라의 고향인 멕시코의 일출을 형상화한 칵테일입니다.</br>상대적으로 무거운 그레나딘 시럽이 가라앉으면서 멋진 그라데이션을 만들어내죠.</br>동명 영화와 이글스의 수록곡 이름으로도 유명합니다."
-        , cocktailColor: "#FF5726, #FFE04F"
+        , cocktailColor: "#FF5726, #FFE04F",
+        glass: "collins",
+        garnish: "체리, 웨지 오렌지"
     },
     {
         name: "톰 콜린스",
@@ -388,7 +414,9 @@ var cocktail_data = [
         ],
         description: "톰 콜린스는 19세기 런던 리마즈 클럽의 수석 웨이터였던 존 콜린스의 작품으로, 네덜란드산 제네바 진(Genéve Gin)을 사용하여 만든 칵테일입니다.</br>처음에는 존 콜린스라고 이름 붙여졌으나, 이후 영국산 올드 톰 진으로 베이스가 바뀌어 현재의 이름으로 바뀌게 되었습니다.</br>제2차 세계대전이 끝난 후 주재료가 지금의 드라이 진으로 바뀌어 더욱 유명해졌으며, 현재는 대부분 런던 드라이진을 사용합니다.</br>소다수의 양을 잘 조절하면 산뜻한 맛을 즐길 수 있습니다."
         ,
-        cocktailColor: "#FFED46, #F8F8F3"
+        cocktailColor: "#FFED46, #F8F8F3",
+        glass: "high-ball",
+        garnish: "체리, 레몬 웨지"
     },
     {
         name: "로시니",
@@ -406,12 +434,14 @@ var cocktail_data = [
         ],
         description: "로시니는 19세기 작곡가 Gioachino조아키노 Rossini의 이름을 따서 만든 칵테일입니다.</br>딸기를 복숭아로 대체하면 bellini칵테일이 되는데 로시니는 이 bellini칵테일의 제일 유명한 대체 칵테일입니다."
         ,
-        cocktailColor: "#E9240C"
+        cocktailColor: "#E9240C",
+        glass: "liqueur",
+        garnish: "딸기"
     },
     {
         name: "미도리 사워",
         answerstring: "020200",
-        oneliner: "작곡가의 영혼이 담긴 음악적 영감",
+        oneliner: "처음에는 달달했던 내 10대의 풋사랑 맛",
         recipe: [
             {
                 ingredName: "미도리 리큐르 60ml",
@@ -427,7 +457,9 @@ var cocktail_data = [
             }
         ],
         description: "미도리 사워는 증류주에 산미와 단맛을 더해 만든 칵테일입니다. '미도리'라는 명칭은 일본말로 '초록'이라는 뜻으로, 멜론을 넣어 만든 술이기에 그 색깔을 본따 지어졌습니다.</br>신맛과 달콤한 맛이 조화로운 맛이며 다양한 과즙과 혼합되면 그 과즙의 맛을 더욱 풍부하게 즐길 수 있으며 나라에 따라 소다수의 용량에 차이가 있습니다.</br>산토리 본사의 허니듀 멜론 술을 이용해 만들어졌습니다. 이 술은 일본에서 자란 최상급 품질의 멜론을 사용해 만들어지는데, 유바리 멜론과 머스크 멜론 두 가지만 사용됩니다.</br>특히 유바리 멜론은 일본에서만 자라는 독특한 품종으로, 풍부한 맛과 과즙을 가진 멜론입니다."
-        , cocktailColor: "#84F26B"
+        , cocktailColor: "#84F26B",
+        glass: "martini",
+        garnish: "레몬 웨지"
     },
     {
         name: "블랙 러시안",
@@ -444,7 +476,9 @@ var cocktail_data = [
             }
         ],
         description: "블랙러시안은 달콤한 커피의 풍미를 가진 칵테일로써 식후에 즐겨도 알맞은 특징을 가지고 있습니다.</br>러시아를 대표하는 보드카가 사용되며 검정색을 띄고 있어 블랙러시안이라는 이름이 붙여지게 되었습니다.</br>커피 리큐어의 단맛이 독한 보드카를 부드럽게 하여, 알코올 함량이 높은데도 불구하고 감칠맛이 좋은 칵테일입니다.</br>블랙러시안에 우유를 넣으면 화이트 러시안으로 즐길 수 있습니다."
-        , cocktailColor: "#5C1F0D"
+        , cocktailColor: "#5C1F0D",
+        glass: "whiskey",
+        garnish: "체리"
     },
     {
         name: "B-52",
@@ -465,7 +499,8 @@ var cocktail_data = [
             }
         ],
         description: "대표적인 슈터칵테일 중 하나로, 폭격기의 강한 이미지와는 다르게 부드러운 맛을 자랑하는 칵테일입니다.</br>소이탄이 발화를 목적으로 쓰이는 탄이라는 점에서 착안하여, 칵테일에도 불을 붙이곤 합니다."
-        , cocktailColor: "#421315, #DDD5C6, #521E19"
+        , cocktailColor: "#421315, #DDD5C6, #521E19",
+        glass: "shot"
     },
     {
         name: "아이리쉬 커피",
@@ -490,6 +525,270 @@ var cocktail_data = [
             }
         ],
         description: "아이리쉬 커피는 커피에 위스키를 넣어만든 칵테일입니다.</br>제2차 세계대전이 끝난 직후, 아일랜드의 국제공항에 위치한 한 술집 주인이 처음 만들어 유명해진 칵테일이라고 합니다.</br>알코올 도수가 높은 위스키가 들어가 차가운 몸을 데워주고 피로를 풀어줍니다.</br>고운 우유 형태의 크림을 올려 부드럽게 마시는 것이 일반적입니다."
-        , cocktailColor: "#521E19, #FEFEFD"
+        , cocktailColor: "#521E19, #FEFEFD",
+        glass: "high-ball",
+        garnish: "아이리쉬 크림"
+    },
+    {
+        name: "로시니",
+        answerstring: "021210",
+        oneliner: "작곡가의 영혼이 담긴 음악적 영감",
+        recipe: [
+            {
+                ingredName: "딸기 퓌레 40ml",
+                ingredColor: "#D64242"
+            },
+            {
+                ingredName: "샴페인 120ml",
+                ingredColor: "#FFEA8E"
+            }
+        ],
+        description: "로시니는 19세기 작곡가 Gioachino조아키노 Rossini의 이름을 따서 만든 칵테일입니다.</br>딸기를 복숭아로 대체하면 bellini칵테일이 되는데 로시니는 이 bellini칵테일의 제일 유명한 대체 칵테일입니다."
+        ,
+        cocktailColor: "#E9240C",
+        glass: "liqueur",
+        garnish: "딸기"
+    },
+    {
+        name: "톰 콜린스",
+        answerstring: "021211",
+        oneliner: "산뜻함을 오래 즐기고 싶다면",
+        recipe: [
+            {
+                ingredName: "진 45ml",
+                ingredColor: "#FDFDFD"
+            },
+            {
+                ingredName: "레몬 주스 30ml",
+                ingredColor: "#FEE251"
+            },
+            {
+                ingredName: "심플 시럽 20ml",
+                ingredColor: "#FDFDFD"
+            },
+            {
+                ingredName: "소다수",
+                ingredColor: "#CCFEFF"
+            }
+        ],
+        description: "톰 콜린스는 19세기 런던 리마즈 클럽의 수석 웨이터였던 존 콜린스의 작품으로, 네덜란드산 제네바 진(Genéve Gin)을 사용하여 만든 칵테일입니다.</br>처음에는 존 콜린스라고 이름 붙여졌으나, 이후 영국산 올드 톰 진으로 베이스가 바뀌어 현재의 이름으로 바뀌게 되었습니다.</br>제2차 세계대전이 끝난 후 주재료가 지금의 드라이 진으로 바뀌어 더욱 유명해졌으며, 현재는 대부분 런던 드라이진을 사용합니다.</br>소다수의 양을 잘 조절하면 산뜻한 맛을 즐길 수 있습니다."
+        ,
+        cocktailColor: "#FFED46, #F8F8F3",
+        glass: "high-ball",
+        garnish: "체리, 레몬 웨지"
+    },
+    {
+        name: "스크리밍 오르가즘",
+        answerstring: "102001",
+        oneliner: "샷을 좋아하지 않는 사람들을 위한 샷",
+        recipe: [
+            {
+                ingredName: "보드카 30ml",
+                ingredColor: "#F7F3E7"
+            },
+            {
+                ingredName: "아이리쉬 크림 30ml",
+                ingredColor: "#FFF7E8"
+            },
+            {
+                ingredName: "아마레또 30ml",
+                ingredColor: "#B52A00"
+            }
+        ],
+        description: "스크리밍 오르가즘은 영화 <칵테일(Cocktail)>을 통해 인기를 끌었던 칵테일인 오르가즘에 보드카를 추가해 만드는 칵테일입니다.</br>달콤한 커피향과 아몬드 리큐어, 부드러운 아이리쉬 크림이 어우러져 달콤하면서 부드러운 맛을 즐길 수 있습니다."
+        , cocktailColor: "#511120, #9C4B06, #CFC5A7, #E6E7DC",
+        glass: "shot"
+    },
+    {
+        name: "핫 샷",
+        answerstring: "102110",
+        oneliner: "모든 걸 잊기위한 화려한 파티에는 이 칵테일을",
+        recipe: [
+            {
+                ingredName: "갈리아노 20ml",
+                ingredColor: "#EDE32F"
+            },
+            {
+                ingredName: "커피 20ml",
+                ingredColor: "#512A00"
+            },
+            {
+                ingredName: "크림 20ml",
+                ingredColor: "#FFF7E8"
+            }
+        ],
+        description: "바닐라 향이 나는 슈터 칵테일입니다.</br>도수가 높으며 한 번에 들이켜야 그 맛을 완전히 느낄 수 있습니다.</br>1987년에 처음 만들어진 이 칵테일은 90년대 초까지 선풍적인 인기를 끌었습니다."
+        ,
+        cocktailColor: "#F8E201, #3F0E17, #F0F0F0",
+        glass: "shot"
+    },
+    {
+        name: "아이리쉬 커피",
+        answerstring: "102111",
+        oneliner: "전쟁이 끝난 자리에 움트는 따뜻함",
+        recipe: [
+            {
+                ingredName: "아이리쉬 위스키 45ml",
+                ingredColor: "#E34E04"
+            },
+            {
+                ingredName: "커피 120ml",
+                ingredColor: "#512A00"
+            },
+            {
+                ingredName: "크림 30ml",
+                ingredColor: "#FFF7E8"
+            },
+            {
+                ingredName: "황설탕 1 티스푼",
+                ingredColor: "#CEA45A"
+            }
+        ],
+        description: "아이리쉬 커피는 커피에 위스키를 넣어만든 칵테일입니다.</br>제2차 세계대전이 끝난 직후, 아일랜드의 국제공항에 위치한 한 술집 주인이 처음 만들어 유명해진 칵테일이라고 합니다.</br>알코올 도수가 높은 위스키가 들어가 차가운 몸을 데워주고 피로를 풀어줍니다.</br>고운 우유 형태의 크림을 올려 부드럽게 마시는 것이 일반적입니다."
+        , cocktailColor: "#521E19, #FEFEFD",
+        glass: "high-ball",
+        garnish: "아이리쉬 크림"
+    },
+    {
+        name: "핫 버터드 럼",
+        answerstring: "110100",
+        oneliner: "추운 겨울날 찬바람을 뚫고 돌아온 벽난로 앞",
+        recipe: [
+            {
+                ingredName: "다크 럼 60ml",
+                ingredColor: "#654323"
+            },
+            {
+                ingredName: "끓는 물 120ml",
+                ingredColor: "#D6FDFF"
+            },
+            {
+                ingredName: "버터 1 티스푼",
+                ingredColor: "#FEFFD6"
+            },
+            {
+                ingredName: "꿀 2 티스푼",
+                ingredColor: "#FFCD07"
+            }
+        ],
+        description: "이탈리아에서 오래전부터 즐겨 마시던 칵테일입니다.</br>럼의 진한 맛과 버터의 고소한 맛이 이루는 조화가 일품으로, 특히 추운 겨울이나 감기 기운이 있을 때 마시기 좋습니다.</br>레시피에서 물 대신 우유를 넣으면 Hot buttered rum cow가 됩니다.",
+        cocktailColor: "#DBA86A",
+        glass: "high-ball",
+        garnish: "슬라이스 레몬, 시나몬 스틱"     
+    },
+    {
+        name: "스키 리프트",
+        answerstring: "110110",
+        oneliner: "설산 위에 내리쬐는 여름의 태양",
+        recipe: [
+            {
+                ingredName: "피치 슈냅스 30ml",
+                ingredColor: "#FFBD60"
+            },
+            {
+                ingredName: "화이트 럼 30ml",
+                ingredColor: "#B2BEAF"
+            },
+            {
+                ingredName: "코코넛 리큐르 15ml",
+                ingredColor: "#E9E7D3"
+            },
+            {
+                ingredName: "핫초코 120ml",
+                ingredColor: "#9E6E50"
+            }
+        ],
+        description: "스키리프트는 특이하게도 핫초코와 럼을 섞어 만드는 칵테일입니다.</br>보통 핫초코하면 초콜릿의 깊은 단 맛부터 떠올리기 쉽지만, 이 칵테일에는 피치 슈냅스와 코코넛 리큐르가 들어가 열대과일의 싱그러운 맛 또한 느낄 수 있습니다."
+        , cocktailColor: "#703712, #D7A168",
+        glass: "high-ball",
+        garnish: "휘핑한 크림"
+    },
+    {
+        name: "블랙벨벳",
+        answerstring: "111100",
+        oneliner: "입속에 펼쳐지는 부드러운 검은 융단",
+        recipe: [
+            {
+                ingredName: "샴페인 45ml",
+                ingredColor: "#FFEA8E"
+            },
+            {
+                ingredName: "흑맥주 45ml",
+                ingredColor: "#211500"
+            }
+        ],
+        description: "블랙벨벳은 1861년 영국 런던의 Brook's club에서 만들어졌습니다.</br> 당시 빅토리아 여왕의 부군이었던 알버트 왕자의 죽음을 애도하기 위해 만들어진 이 칵테일은 조문객들이 착용하는 검은 완장을 형상화한 것이라고 합니다.</br> 샴페인의 청량감에 스타우트 흑맥주의 쌉싸름함과 깊은 풍미가 느껴지는 고급스러운 맛을 느껴보세요.",
+        cocktailColor: "#F4DD7C, #430B0E",
+        glass: "liqueur"
+    },
+    {
+        name: "쿠바 리브레",
+        answerstring: "111101",
+        oneliner: "Viva Cuba Libre!",
+        recipe: [
+            {
+                ingredName: "쿠바산 럼 45ml",
+                ingredColor: "#DD8108"
+            },
+            {
+                ingredName: "콜라 150ml",
+                ingredColor: "#330000"
+            },
+            {
+                ingredName: "라임 주스 10ml",
+                ingredColor: "#C0D643"
+            }
+        ],
+        description: "쿠바리브레라는 이름은 스페인의 식민지였던 쿠바가 독립운동 당시 사용하던 \"Viva Cuba Libre(자유 쿠바 만세)\"라는 구호에서 유래되었습니다.</br> 쿠바의 독립전쟁을 지원하기 위해 하바나에 주둔하던 미군이 승리를 축하하며 쿠바산 럼에 미국산 콜라를 넣어 마신 것에서 탄생했습니다.</br> 럼 특유의 달콤한 향기에 콜라의 단맛과 레몬의 신맛이 가미되어 상큼함을 더해진 맛입니다.",
+        cocktailColor: "#843118",
+        glass: "high-ball",
+        garnish: "라임 웨지"
+    },
+    {
+        name: "프렌치75",
+        answerstring: "111110",
+        oneliner: "전쟁의 비극이 탄생시킨 진정한 폭탄주",
+        recipe: [
+            {
+                ingredName: "진 30ml",
+                ingredColor: "#FDFDFD"
+            },
+            {
+                ingredName: "레몬 주스 20ml",
+                ingredColor: "#FEE251"
+            },
+            {
+                ingredName: "심플 시럽 20ml",
+                ingredColor: "#FDFDFD"
+            },
+            {
+                ingredName: "샴페인",
+                ingredColor: "#FFEA8E"
+            }
+        ],
+
+        description: "프렌치75는 제1차 세계대전 중 파리에서 탄생한 칵테일입니다. 그 이름은 당시 프랑스에서 개발된 1897년식 75mm 대포에서 따왔습니다.</br>샴페인이 들어가며 약간 단맛을 느낄 수 있습니다.</br>베이스로 진 대신 버본 위스키를 사용할 경우 french 95, 브랜디를 사용할 경우 french 125 라는 칵테일로 즐길 수 있습니다."
+        ,
+        cocktailColor: "#FAE79A",
+        glass: "liqueur",
+        garnish: "레몬 트위스트"
+    },
+    {
+        name: "오후의 죽음",
+        answerstring: "111111",
+        oneliner: "오전에 먹다가 오후에 죽어도 모를 오묘함",
+        recipe: [
+            {
+                ingredName: "압생트 15ml",
+                ingredColor: "#38BC43"
+            },
+            {
+                ingredName: "샴페인 45ml",
+                ingredColor: "#FFEA8E"
+            }
+        ],
+        description: "오후의 죽음은 빈센트 반 고흐가 사랑했던 술로 유명한 압생트와 샴페인을 섞어 만드는 칵테일입니다.</br>미국의 문학가 헤밍웨이가 고안한 칵테일로, 자신의 단편인 「오후의 죽음」에서 이름을 따온 것으로 알려집니다.</br>압생트의 산초향과 샴페인의 청량감이 함께 느껴지는 오묘한 맛을 즐길 수 있습니다."
+        , cocktailColor: "#9AF193, #DEEF85",
+        glass: "liqueur"
     }
 ]
